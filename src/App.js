e31581b65a2e168d6fb021d6eed5a6d14cbc2581
yukselsule <sulecantekin@gmail.com>
2024-05-23 16:58:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="app">
+        <Form />
+        <List />
+        <CheckedList />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return <h1>My List</h1>;
+}
+
+function Form() {
+  return (
+    <div className="form">
+      <Button>Add list item</Button>
+      <div className="form-add">
+        <input type="text" placeholder="list item"></input>
+        <Button>Add</Button>
+      </div>
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <div className="list">
+      <div>
+        <span>Check list</span>
+        <Button> &#x2B9F;</Button>
+      </div>
+
+      <ul>
+        <Item />
+      </ul>
+    </div>
+  );
+}
+
+function CheckedList() {
+  return (
+    <div className="checked-list">
+      <div>
+        <span>X checked items</span>
+        <Button>&#x2B9F;</Button>
+      </div>
+
+      <ul>
+        <Item />
+      </ul>
+    </div>
+  );
+}
+
+function Item() {
+  return (
+    <div className="item">
+      <input type="checkbox" />
+      <span>List item</span>
+      <Button>&#x2716;</Button>
+    </div>
+  );
+}
+
+function Button({ children }) {
+  return <button>{children}</button>;
+}
